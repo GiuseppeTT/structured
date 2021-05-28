a3853834e94bfd9d3e62ae96ac94ffcda75e08be
Giuseppe Tinti Tomio <67562667+GiuseppeTT@users.noreply.github.com>
2021-05-28 14:37:54 +0000

    Add base structure (#1)

---
+++ R/create-project.R
@@ -0,0 +1,7 @@
+#' @title
+#' Create project
+#'
+#' @export
+create_project <- function(
+) {
+}
---
+++ R/structured-package.R
@@ -0,0 +1,17 @@
+#' @title
+#' structured: A Tool To Create Structured Projects
+#'
+#' @description
+#' A tool to create structured projects. The projects can be created either
+#' interactively using the RStudio Project Templates feature, or
+#' programmatically using the [create_project] function exported by this
+#' package.
+#'
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
