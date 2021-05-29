56183d3735bcf0d3bba2c08d398bc48e7fc26bd9
GiuseppeTT <giu.tinti@gmail.com>
2021-05-29 01:11:51 +0000

    Complete template test

--- tests/testthat/test-structure-project.R
+++ tests/testthat/test-structure-project.R
@@ -30,14 +30,17 @@ test_that("There are no template files", {
         )
         expect_length(name_template_files, 0)
 
-        project_files <- fs::dir_ls(
+        fs::dir_map(
             project_path,
+            fun = function(path) {
+                file_content <- read_file(path)
+                template_matches <- grep("\\{\\{.*\\}\\}", file_content)
+                expect_length(template_matches, 0)
+            },
             all = TRUE,
             recurse = TRUE,
             type = "file"
         )
-        content_template_files <- character()
-        expect_length(content_template_files, 0)
     })
 })
 
