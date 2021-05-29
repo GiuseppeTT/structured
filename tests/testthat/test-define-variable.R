114d2a80ba723f7dd90e7cd2f2a63ff0b6ca4c58
GiuseppeTT <giu.tinti@gmail.com>
2021-05-29 01:50:11 +0000

    Add define_variables tests

---
+++ tests/testthat/test-define-variable.R
@@ -0,0 +1,11 @@
+test_that("get_project_slug works", {
+    project_path <- "test/long/path/my-project"
+
+    expect_equal(get_project_slug(project_path), "my-project")
+})
+
+test_that("get_project_name works", {
+    project_path <- "test/long/path/my-project"
+
+    expect_equal(get_project_name(project_path), "my project")
+})
