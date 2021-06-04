255d6327a5c08fa5cdd3feb4a4981f0d919ab94e
GiuseppeTT <giu.tinti@gmail.com>
2021-06-04 15:07:27 +0000

    Add Targets analysis format

--- R/define-variables.R
+++ R/define-variables.R
@@ -6,7 +6,8 @@ define_variables <- function(
     variables <- list(
         project_slug = get_project_slug(project_path),
         project_name = get_project_name(project_path),
-        how_to_run = get_how_to_run(analysis_format)
+        how_to_run = get_how_to_run(analysis_format),
+        ignored_files = get_ignored_files(analysis_format)
     )
 
     return(variables)
@@ -37,9 +38,26 @@ get_how_to_run <- function(
         "Script" = "run script `analysis.R`",
         "R Markdown (Simplified)" = "knit document `analysis.Rmd`",
         "R Markdown" = "run script `render.R`",
+        "Targets" = "execute command `targets::tar_make()`",
         "Shiny" = "run script `app.R`",
         stop("Invalid analysis format")
     )
 
     return(how_to_run)
+}
+
+get_ignored_files <- function(
+    analysis_format
+) {
+    ignored_files <- switch(
+        analysis_format,
+        "Script" = "",
+        "R Markdown" = "",
+        "R Markdown (Structured)" = "",
+        "Targets" = "_targets/",
+        "Shiny" = "",
+        stop("Invalid analysis format")
+    )
+
+    return(ignored_files)
 }
