ba50c187d5dedf0574cc0c0d8d61abee3daa0e86
GiuseppeTT <giu.tinti@gmail.com>
2021-07-03 17:43:14 +0000

    Try fix

--- R/read-write-file.R
+++ R/read-write-file.R
@@ -1,12 +1,18 @@
 read_file <- function(
     path
 ) {
-    return(readChar(path, file.info(path)$size))
+    #return(readChar(path, file.info(path)$size))
+    lines <- readLines(path)
+    content <- paste(lines, collapse = "\n")
+    content <- paste0(content, "\n")
+
+    return (content)
 }
 
 write_file <- function(
     path,
     content
 ) {
-    cat(content, file = path)
+    #cat(content, file = path)
+    writeLines(content, path, sep = "")
 }
