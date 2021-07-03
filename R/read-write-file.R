7970050d870bebce97c828057649964b95167b17
GiuseppeTT <giu.tinti@gmail.com>
2021-07-03 17:50:21 +0000

    Try improved fix

--- R/read-write-file.R
+++ R/read-write-file.R
@@ -2,9 +2,15 @@ read_file <- function(
     path
 ) {
     #return(readChar(path, file.info(path)$size))
+
+    if (.Platform$OS.type == "windows")
+        next_line <- "\r\n"
+    else
+        next_line <- "\n"
+
     lines <- readLines(path)
-    content <- paste(lines, collapse = "\n")
-    content <- paste0(content, "\n")
+    content <- paste(lines, collapse = next_line)
+    content <- paste0(content, next_line)
 
     return (content)
 }
